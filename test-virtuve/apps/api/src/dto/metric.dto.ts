1123248475d00ff80997723c35349a09a6d5e975
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-10-13 12:30:39 +0200

    rm unused import + packagejson type module

--- test-virtuve/apps/api/src/dto/metric.dto.ts
+++ test-virtuve/apps/api/src/dto/metric.dto.ts
@@ -1,6 +1,6 @@
-import { IsDate, IsDateString, IsEnum, IsInt, IsNumber, IsOptional, Length, Max, Min } from 'class-validator';
+import { IsDateString, IsEnum, IsInt, IsNumber, IsOptional, Length, Max, Min } from 'class-validator';
 import { MetricType } from '@virtuve/biz-core/value_objects';
-import { Transform, Type } from 'class-transformer';
+import { Transform } from 'class-transformer';
 import { IsValidDateRange } from 'src/utils/validation';
 import { AthleteMetricsAggregateOperation } from '@virtuve/biz-core/queries/metrics/get_athlete_metrics_aggregate_query';
 
