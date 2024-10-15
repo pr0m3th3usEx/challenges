e928af4f1a0a630e1445f96dc34e813afc2b7c58
pr0m3th3usEx <thomas1.michel@epitech.eu>
2024-10-15 20:22:02 +0200

    add github actions

--- test-virtuve/apps/api/src/index.ts
+++ test-virtuve/apps/api/src/index.ts
@@ -26,7 +26,7 @@ app.use(
 
 app.use(customLogger);
 
-// Deployment (Docker, Terraform)
+// Deployment (Github Actions, Terraform)
 // Caching (Redis)
 // Monitoring (Grafana, Prometheus)
 // Documentation (Swagger, Readme)
